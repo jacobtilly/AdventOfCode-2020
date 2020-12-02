e1a2fa1d8d0eb203dd7070a7bac0de979d37636d
Jacob Tilly <dev@jacobtilly.com>
2020-12-02 11:35:30 +0100

    Solution part 1

--- day1/part1.py
+++ day1/part1.py
@@ -1,2 +1,6 @@
-f = open("input.txt", "r")
-print(f.read())
+f = open("/Users/jacobtilly/Documents/GitHub/AdventOfCode-2020/day1/input.txt", "r")
+input = f.read().split('\n')
+for a in input:
+  for b in input:
+      if(2020-int(a)-int(b) == 0):
+          print(int(a)*int(b))
