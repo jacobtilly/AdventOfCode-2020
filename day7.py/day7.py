08f51f19210cbd302d76a9e34085ec2f96e935e2
Jacob Tilly <dev@jacobtilly.com>
2020-12-06 22:51:25 +0100

    prepared for day 7

---
+++ day7.py/day7.py
@@ -0,0 +1,10 @@
+def getInput(filename):
+    file = open("/Users/jacobtilly/Documents/GitHub/AdventOfCode-2020/day6/"+filename, "r")
+    return file.read().split("\n") # change this row to format code better
+
+# functions below
+
+
+result = "" # change this row
+
+print(result)
